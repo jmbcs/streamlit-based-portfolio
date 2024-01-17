5b1edea263946b8eb2fc1dc05fc90aee630f2080
jmbcs <julio.m.b.c.silva+github@gmail.com>
2024-01-17 15:10:12 +0000

    moved button tabs to tabs folder

---
+++ portfolio/tabs/contact.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from settings import config
+from streamlit_lottie import st_lottie
+
+
+def set_section():
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("...")
+        with col2:
+            st_lottie(config.animations.contact, height=700, width=900)
