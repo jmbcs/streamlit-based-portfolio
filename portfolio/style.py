362ed37df9504f31bcefa67046c440dc3a84c081
juliosilva000 <jcosta@wavecom.pt>
2024-01-17 15:11:39 +0000

    using pydantic as configuration

--- portfolio/style.py
+++ portfolio/style.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 import streamlit as st
+from settings import config
 
 
-def set_style(path_to_style: Path):
+def set_style(path_to_style: Path = config.style_path):
     # CSS styles file
-    with open(path_to_style) as f:
+    with open(path_to_style, mode="r") as f:
         st.write(f"<style>{f.read()}</style>", unsafe_allow_html=True)
