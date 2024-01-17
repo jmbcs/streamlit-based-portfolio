495eca5c933617a34b1ccb6ae706ba1f2b209bad
jmbcs <julio.m.b.c.silva+github@gmail.com>
2024-01-17 16:43:01 +0000

    small updates

--- portfolio/style.py
+++ portfolio/style.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 
 import streamlit as st
-from settings import config
 
 
-def set_style(path_to_style: Path = config.style_path):
+def set_style(path_to_style: Path):
     # CSS styles file
     with open(path_to_style, mode="r") as f:
         st.write(f"<style>{f.read()}</style>", unsafe_allow_html=True)
