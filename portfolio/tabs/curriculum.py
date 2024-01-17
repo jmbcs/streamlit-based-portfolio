38e6949c0d8937c34225623782b1c70849bf2baa
juliosilva000 <jcosta@wavecom.pt>
2024-01-17 23:30:51 +0000

    improved margin of pdf

--- portfolio/tabs/curriculum.py
+++ portfolio/tabs/curriculum.py
@@ -8,8 +8,8 @@ def __get_pdf_display():
     with open(config.curriculum_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode("utf-8")
 
-    # Embedding PDF in HTML
-    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="1750" height="1200" type="application/pdf">'
+    # Adding left and right margins to the PDF display
+    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="1320" height="1200" type="application/pdf" style="margin-left: 220px; margin-right: 215px;">'
     return pdf_display
 
 
