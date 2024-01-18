e5f6c5c34c47a4667f2dfe0183208116ec3fcff8
jmbcs <julio.m.b.c.silva+gitlab@gmail.com>
2024-01-18 10:52:21 +0000

    implemented html files directly on code using settings.py

--- portfolio/__main__.py
+++ portfolio/__main__.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from settings import config
 
 import portfolio.buttons
 import portfolio.image
 import portfolio.style
+from portfolio.settings import config
 
 
 def run():
@@ -11,19 +11,21 @@ def run():
         page_title="Porfolio - Júlio Silva", page_icon="💼", layout="wide"
     )
 
+    # set the styles
     portfolio.style.set_style(path_to_style=config.styles.main)
+    st.markdown(config.html.style, unsafe_allow_html=True)
 
     portfolio.image.set_profile_image()
 
     # Top title
     st.markdown(
-        "<h1 style='text-align: center; color: #ffffff;'>Júlio Silva</h1>",
+        config.html.title,
         unsafe_allow_html=True,
     )
 
     # Subtitle
     st.write(
-        f"""<div class="subtitle" style="text-align: center;color: #ff4c4c;">Backend Developer <span style="color: #ffffff;">- </span>Data Analyst</div>""",
+        config.html.subtitle,
         unsafe_allow_html=True,
     )
 
@@ -31,16 +33,13 @@ def run():
 
     # Displaying Markdown with icons
 
-    divider_html = '<div style="margin-left: 215px; margin-right: 215px; margin-top: 20px; margin-bottom: 20px; border-top: 2px solid #888;"></div>'
-    st.markdown(divider_html, unsafe_allow_html=True)
+    st.markdown(config.html.divider, unsafe_allow_html=True)
 
     portfolio.buttons.set_button_bar()
 
-    st.markdown(divider_html, unsafe_allow_html=True)
+    st.markdown(config.html.divider, unsafe_allow_html=True)
 
-    text_html = "<div style='text-align: center; color: #ffffff;'>If you liked my portfolio, feel free to check my code at :</div>"
-
-    st.markdown(text_html, unsafe_allow_html=True)
+    st.markdown(config.html.footer, unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
