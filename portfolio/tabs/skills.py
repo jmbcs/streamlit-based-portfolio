e625d05b3dd69b9da5780301ad40e3608efdea56
jmbcs <julio.m.b.c.silva+github@gmail.com>
2024-01-18 16:12:31 +0000

    using html for skills now

--- portfolio/tabs/skills.py
+++ portfolio/tabs/skills.py
@@ -5,32 +5,6 @@ from streamlit_lottie import st_lottie
 import portfolio.buttons
 
 
-def __insert_skill(skill_type: str, skills: str):
-    col1, col2 = st.columns([1, 2])
-    with col1:
-        st.markdown(
-            f"<div style='margin-left: 12.5vw; margin-right: 12.5vw; margin-bottom: 1vh; font-size: 14px; line-height: 2.5; white-space: nowrap;'><strong>{skill_type}</strong> </div>",
-            unsafe_allow_html=True,
-        )
-
-    with col2:
-        split_skills = [skill.strip() for skill in skills.split(",")]
-
-        skill_snippets = " ".join(
-            [
-                f"<code style='color: #ff4c4c; font-size: 14px; line-height: 2.5;'>{skill}</code>"
-                if (i + 1) % 3 != 0
-                else f"<code style='color: #ff4c4c; font-size: 14px; line-height: 2.5;'>{skill}</code><br>"
-                for i, skill in enumerate(split_skills)
-            ]
-        )
-
-        st.markdown(
-            f"<div style='margin-left: 12.5vw; margin-bottom: 3vh'>{skill_snippets}</div>",
-            unsafe_allow_html=True,
-        )
-
-
 def set_section():
     with st.container():
         col1, col2 = st.columns(2)
@@ -43,37 +17,10 @@ def set_section():
             )
 
             portfolio.buttons.set_tech_stack_icons()
-
             st.markdown(
-                """
-                <h2 style='margin-left: 11.5vw;'>Tech Skills</h2>
-                """,
+                config.html.skills,
                 unsafe_allow_html=True,
             )
-            __insert_skill("Programming", "Python, Golang, Bash")
-            __insert_skill(
-                "Databases",
-                "SQL, NoSQL, PostegreSQL, Victoriametrics, Promscale, Prometheus, Elasticsearch, PromQL, pgAdmin",
-            )
-            __insert_skill("Dashboading", "Grafana, Kibana")
-            __insert_skill("Containerization", "Docker, Docker Compose")
-            __insert_skill("Data formats", "JSON, CSV, Protobuf, YAML")
-            __insert_skill("Others", "Git, Telegraf, Linux, direnv, Makefile")
-
-            st.markdown(
-                """
-                <h2 style='margin-left: 11.5vw;'> Python Skills</h2>
-                """,
-                unsafe_allow_html=True,
-            )
-
-            __insert_skill(
-                "Software Development",
-                "FastAPI, SQLAlchemy, Aiohttp, Pydantic, Requests",
-            )
-            __insert_skill("Web Development", "Streamlit")
-            __insert_skill("Data Analytics", "Pandas, Numpy, Matplotlib, Tensorflow")
-            __insert_skill("Computer Vision", "ImageAI, OpenCV, Ultralytics")
 
         with col2:
-            st_lottie(config.animations.skills)
+            st_lottie(config.animations.skills, quality="high")
