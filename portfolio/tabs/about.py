131acb8f19a9c7d313c14d3e948c08f62cb9834c
jmbcs <julio.m.b.c.silva+gitlab@gmail.com>
2024-01-21 02:08:45 +0000

    general update in images, ui, fonts

--- portfolio/tabs/about.py
+++ portfolio/tabs/about.py
@@ -2,6 +2,8 @@ import streamlit as st
 from settings import config
 from streamlit_lottie import st_lottie
 
+import portfolio.buttons
+
 
 def set_section():
     with st.container():
@@ -11,6 +13,13 @@ def set_section():
                 config.html.about,
                 unsafe_allow_html=True,
             )
+            # st.markdown(
+            #     """
+            #     <h2 style='margin-left: 11.5vw;'>My Socials</h2>
+            #     """,
+            #     unsafe_allow_html=True,
+            # )
+            # portfolio.buttons.set_social_icons()
 
         with col2:
             st_lottie(config.animations.about, quality="high")
