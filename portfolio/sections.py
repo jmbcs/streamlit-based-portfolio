7dad6cb63e7a4bc68256c6af50321421d6252c21
jmbcs <julio.m.b.c.silva+github@gmail.com>
2024-01-21 15:51:51 +0000

    testing new pdf placement

--- portfolio/sections.py
+++ portfolio/sections.py
@@ -14,10 +14,12 @@ def __get_pdf_display() -> str:
     with open(config.curriculum_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode("utf-8")
 
-    pdf_display = f"""<object data="data:application/pdf;base64, {base64_pdf}" type="application/pdf" width="75%" height="1080" style="margin-left: auto; margin-right: auto; display: block;">
-        <p style="text-align: center;">Your browser does not support embedded PDF files.</p> 
-        <p style="text-align: center;">You can download my curriculum <a href="{url_link}" style="color: #ff4c4c;" target="_blank">here</a> instead.</p>
-    </object>"""
+    pdf_display = f"""<div style="text-align: center;">
+                        <object data="data:application/pdf;base64, {base64_pdf}" type="application/pdf" width="750vw" height="750vh display: flex;">
+                            <p>Your browser does not support embedded PDF files.</p> 
+                            <p>You can download my curriculum <a href="{url_link}" style="color: #ff4c4c;" target="_blank">here</a> instead.</p>
+                        </object>
+                    </div>"""
 
     return pdf_display
 
